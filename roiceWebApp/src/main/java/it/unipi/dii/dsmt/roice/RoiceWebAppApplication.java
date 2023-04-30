6b87e258674de1e9defc38237258dd008f396cc6
LucT3 <luc.tart3@gmail.com>
2023-04-30 18:09:23 +0200

    first commit

---
+++ roiceWebApp/src/main/java/it/unipi/dii/dsmt/roice/RoiceWebAppApplication.java
@@ -0,0 +1,14 @@
+package it.unipi.dii.dsmt.roice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoiceWebAppApplication {
+
+    public static void main(String[] args) {
+        // Start-string
+        SpringApplication.run(RoiceWebAppApplication.class, args);
+    }
+
+}
