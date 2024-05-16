d21a55853d13857f37e21870344f8c2dd4628ae5
Salvatore Lombardi <74710148+salbh@users.noreply.github.com>
2024-05-16 16:05:09 +0200

    Fixed won auction page update

--- roiceWebApp/src/main/java/it/unipi/dii/dsmt/roice/controller/UserPageController.java
+++ roiceWebApp/src/main/java/it/unipi/dii/dsmt/roice/controller/UserPageController.java
@@ -1,12 +1,22 @@
 package it.unipi.dii.dsmt.roice.controller;
 
 import it.unipi.dii.dsmt.roice.dto.UserDTO;
+import it.unipi.dii.dsmt.roice.dto.mapper.UserMapper;
+import it.unipi.dii.dsmt.roice.model.GenericUser;
+import it.unipi.dii.dsmt.roice.model.User;
+import it.unipi.dii.dsmt.roice.service.UserService;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Optional;
+
 @Controller
 public class UserPageController {
+
+    @Autowired
+    private UserService userService;
 
     @GetMapping("/userPage")
     public String userPage(HttpSession session) {
@@ -14,6 +24,10 @@ public class UserPageController {
         if (currentUser == null) {
             return "redirect:/login";
         }
+        Optional<GenericUser> genericUser = userService.findByEmail(currentUser.getEmail());
+        User user = (User) genericUser.get();
+        UserDTO userDTO = UserMapper.toUserDTO(user);
+        session.setAttribute("currentUser", userDTO);
 
         return "userPage";
     }
